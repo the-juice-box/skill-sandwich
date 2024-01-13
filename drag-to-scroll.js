ca0cef0bb0fa0a8a6eea6e37849d3927a6c44a5e
andrewens <andrewens4@gmail.com>
2024-01-13 10:19:01 -0600

    Can grab and scroll web page in two dimensions

--- drag-to-scroll.js
+++ drag-to-scroll.js
@@ -1,49 +1,57 @@
 const box = document.querySelector(".tech-tree");
+const body = document.querySelector("body");
 
 let isDown = false;
-let startX;
+let startX; // position of mouse, fixed to screen
 let startY;
-let scrollLeft;
+let scrollLeft; // initial scroll position
 let scrollTop;
 
-box.addEventListener("mousedown", (e) => {
-  console.log("DOWN " + box.scrollTop);
+function debugDot(x, y) {
+  const pt = document.createElement("div");
+  pt.style.position = "fixed";
+  pt.style.left = x + 5 + "px";
+  pt.style.top = y + 5 + "px";
+  pt.style.width = "10px";
+  pt.style.height = "10px";
+  pt.style.background = "#000000";
+  pt.id = x + " " + y;
+  box.appendChild(pt);
+  return pt;
+}
 
+// mouse events
+function mouseDown(e) {
   isDown = true;
-  startX = e.pageX - box.offsetLeft;
-  startY = e.pageY - box.offsetTop;
 
-  console.log("pageY " + e.pageY);
-  scrollLeft = box.scrollLeft;
-  scrollTop = box.scrollTop;
-  box.style.cursor = "grabbing";
-});
+  scrollLeft = window.scrollX;
+  scrollTop = window.scrollY;
 
-box.addEventListener("mouseleave", () => {
+  startX = e.pageX - box.offsetLeft - scrollLeft;
+  startY = e.pageY - box.offsetTop - scrollTop;
+
+  box.style.cursor = "grabbing"; // this doesn't work very well :(
+}
+function mouseUp(e) {
   isDown = false;
   box.style.cursor = "grab";
-});
-
-box.addEventListener("mouseup", () => {
-  isDown = false;
-  box.style.cursor = "grab";
-});
-
-document.addEventListener("mousemove", (e) => {
+}
+function mouseMove(e) {
   if (!isDown) return;
-  console.log("MOVE");
   e.preventDefault();
-  const x = e.pageX - box.offsetLeft;
-  const y = e.pageY - box.offsetTop;
 
-  console.log("y start " + startY);
-  const walkX = (x - startX) * 0.01; // Change this number to adjust the scroll speed
-  const walkY = (y - startY) * 0.01; // Change this number to adjust the scroll speed
+  const x = e.pageX - box.offsetLeft - window.scrollX;
+  const y = e.pageY - box.offsetTop - window.scrollY;
 
-  window.scrollBy(walkX, walkY);
-  //console.log("scrollLeft " + box.scrollLeft);
-  //box.scrollLeft += walkX; // these don't do anything
- // box.scrollTop += walkY; // https://stackoverflow.com/questions/76066584/how-to-enable-touch-like-scrolling-by-grabbing-and-dragging-with-the-mouse
+  const newScrollX = scrollLeft - x + startX;
+  const newScrollY = scrollTop - y + startY;
 
-  console.log("Scroll top " + (scrollTop - walkY));
-});
+  window.scrollTo(newScrollX, newScrollY);
+ }
+
+box.addEventListener("mousedown", mouseDown);
+box.addEventListener("mouseleave", mouseUp);
+box.addEventListener("mouseup", mouseUp);
+document.addEventListener("mousemove", mouseMove);
+
+mouseUp();
